c6b096d9b10c1cbbb2d774e47bb9667be3bd463e
maxmora10 <max.mora@enti.cat>
2025-01-11 16:49:32 +0100

    main.py

--- main.py
+++ main.py
@@ -21,3 +21,36 @@ def generate_password():
         entry_password.insert(0, password)
     except ValueError as e:
         messagebox.showerror("Error",f"Entrada no valida: {e}")
+
+#funcion para comprobar la seguridad de una contraseña
+def check_password():
+    
+    password = entry_password.get()
+    # se obtiene la contraseña puesta por el usuario
+    if len(password) < 8:
+        # comprueba si la longitud de la contraseña es inferior a 8 caracteres
+        # si es así , muestra un mensaje diciendo que la contraseña es débil
+        messagebox.showinfo("Comprobación", "La contraseña es débil: menos de 8 caracteres.")
+        
+    elif not any(char.isdigit() for char in password):
+        #comprueba si la contraseña no tienen ningun numero
+        # verifica si al menos un carácter es un dígito
+        # si no hay numeros, muestra un mensaje diciendo que la contraseña es débil
+        messagebox.showinfo("Comprobación", "La contraseña es débil: no contiene números.")
+        
+    elif not any(char.isupper() for char in password):
+        # comprueba si la contraseña no tiene mayusculas
+        # verifica si al menos un caracter es una mayúscula
+        # si no hay mayuscula, muestra un mensaje diciendo que la contraseña es débil
+        messagebox.showinfo("Comprobación", "La contraseña es débil: no contiene letras mayúsculas.")
+        
+    elif not any(char in string.punctuation for char in password):
+        # comprueba si la contraseña no tiene caracteres especiales
+        # verifica si al menos un caracter de la contraseña  pertenece a 'string punctuation'
+        # si no hay caracteres especiales, muestra un mensaje diciendo que la contraseña es débil
+        messagebox.showinfo("Comprobación", "La contraseña es débil: no contiene caracteres especiales.")
+        
+    else:
+        messagebox.showinfo("Comprobación", "La contraseña es segura.")
+        # si la contraseña cumple con todos los requisitos (longitud minima, contiene numeros, letras mayusculas y caracteres especiales), es segura.
+        # muestra un mensaje diciendo que la contraseña es segura
